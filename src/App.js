d78c64029da350f98edd45919ddbfef7cceced63
Yusuf Akbulut <80647654+ysfkblt@users.noreply.github.com>
2022-02-14 23:25:30 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const APP_ID = '5e158d7c';
+	const APP_KEY = '5015b6c1e1952e2042095330e6e87b07';
+
+	const [rcps, setrcps] = useState([]);
+	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('chicken');
+
+	useEffect(() => {
+		gtrcps();
+	}, [query]);
+
+	const gtrcps = async () => {
+		const rspns = await fetch(
+			`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+		);
+		const data = await rspns.json();
+		setrcps(data.hits);
+	};
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	function getSearch(e) {
+		e.preventDefault();
+		setQuery(search);
+		setSearch('');
+	}
+
+	return (
+		<div className='App'>
+			<form onSubmit={getSearch} className='search-form'>
+				<input
+					className='search-bar'
+					type='text'
+					value={search}
+					onChange={updateSearch}
+				/>
+				<button className='search-button' type='submit'>
+					Search
+				</button>
+			</form>
+			<div className='recipes'>
+				{rcps.map((rcp) => (
+					<Recipe
+						key={rcp.recipe.label}
+						title={rcp.recipe.label}
+						calories={rcp.recipe.calories}
+						image={rcp.recipe.image}
+						ingredients={rcp.recipe.ingredients}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
